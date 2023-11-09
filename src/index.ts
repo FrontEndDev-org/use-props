3cdfdc4718730bda0bbf3da06209bcc8edc42a8d
#云淡然 <hi@ydr.me>
2023-11-09 10:59:31 +0800

    feat: 类型优化

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import classNames, { type Argument as ClassName } from 'classnames';
-import type { CSSProperties, ReactNode, HTMLAttributes } from 'react';
+import type { CSSProperties, HTMLAttributes } from 'react';
+
 export * from './const.js';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AnyObject = Record<keyof any, any>;
-export type Props<P extends AnyObject = AnyObject, E extends HTMLElement = HTMLElement> = {
-  className?: ClassName;
-  style?: CSSProperties & Record<string, string>;
-  children?: ReactNode;
-} & HTMLAttributes<E> &
-  P;
-export type RuntimeProps<P extends AnyObject, D extends Partial<P>> = Omit<P, keyof D> & Required<Pick<P, keyof D>>;
+type NormalProps = {
+  className?: ClassName | undefined;
+  style?: (CSSProperties | Record<string, string | number>) | undefined;
+};
+type NormalKeys = keyof NormalProps;
+
+export type Props<P extends AnyObject = AnyObject, E = HTMLAttributes<HTMLElement>> =
+  // html 属性里包含 children、className、style
+  Omit<E, keyof P | NormalKeys> &
+    // 普通属性里包含 className、style
+    Omit<NormalProps, keyof P> &
+    P;
+
+type NormalRuntimeProps = {
+  className?: string | undefined;
+  style?: CSSProperties | undefined;
+};
+export type RuntimeProps<P extends AnyObject, D extends Partial<P>> = Omit<P, keyof D | NormalKeys> &
+  Required<Pick<P, keyof D | NormalKeys>> &
+  NormalRuntimeProps;
 
 export default function useProps<P extends Props, D extends Partial<P>>(props: P, defaults: D): RuntimeProps<P, D> {
   const target = Object.assign({}, props) as RuntimeProps<P, D>;
